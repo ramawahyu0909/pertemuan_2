403f1f8b42048bef5231bbfa4bf038c9fcab0e0e
Rama Wahyu Nugraha <ramawahyunugraha120@gmail.com>
2026-09-07 11:23:00 +0700

    Implement basic MVC structure

--- belajar-laravel/routes/web.php
+++ belajar-laravel/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// 1. Import controller yang sudah dibuat
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MemberController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 2. Tambahkan route untuk MVC pertemuan 5
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/members', [MemberController::class, 'index']);
