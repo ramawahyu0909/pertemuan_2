6a9d48b40d4cda5e048d637c7324735bca2c20fa
Rama Wahyu Nugraha <ramawahyunugraha120@gmail.com>
2026-09-12 21:38:42 +0700

    Clean up comments in routes

--- belajar-laravel/routes/web.php
+++ belajar-laravel/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-// 1. Import controller yang sudah dibuat
+
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\MemberController;
@@ -10,7 +10,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// 2. Tambahkan route untuk MVC pertemuan 5
+
 Route::get('/books', [BookController::class, 'index']);
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/members', [MemberController::class, 'index']);
